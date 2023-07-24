4d7784536e7f13e518de28a80b32630e9f3c0296
Arthur Kabera <arthurkabera@gmail.com>
2023-07-25 02:35:10 +0300

    Login Working

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,63 @@
+import { useState, useEffect } from "react";
 import logoUrl from "../../assets/images/logo.svg";
 import DarkModeSwitcher from "../../components/DarkModeSwitcher";
 import MainColorSwitcher from "../../components/MainColorSwitcher";
 import Button from "../../base-components/Button";
 import { FormInput, FormCheck } from "../../base-components/Form";
+import { useNavigate } from "react-router-dom";
+import LoadingIcon from "../../base-components/LoadingIcon";
+
+import { db, auth } from "../../../firebaseConfig";
+import firebase from "firebase/compat/app";
 
 function Main() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        // User is signed in, redirect to a different page (let's say '/dashboard')
+        navigate("/");
+      } else {
+        // No user is signed in, remain on the current page or redirect to a specific page
+        // history.push('/login');
+      }
+    });
+  }, [navigate]);
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const navigateToSignUp = () => {
+    navigate("/register");
+  };
+
+  const handleSignIn = async (event: any) => {
+    setIsLoading(true);
+    event.preventDefault();
+    try {
+      await firebase.auth().signInWithEmailAndPassword(email, password);
+      console.log("User signed in successfully");
+      // Navigate to Dashboard
+      navigate("/");
+    } catch (err) {
+      // Handle Errors here.
+      if (err instanceof Error) {
+        // check that err is an instance of Error
+        console.log(err.message);
+      } else {
+        console.log(err);
+      }
+    }
+    setIsLoading(false);
+  };
+
   return (
     <>
       <div className="container">
@@ -23,14 +76,24 @@ function Main() {
             <div className="box px-5 py-8 mt-10 max-w-[450px] relative before:content-[''] before:z-[-1] before:w-[95%] before:h-full before:bg-slate-200 before:border before:border-slate-200 before:-mt-5 before:absolute before:rounded-lg before:mx-auto before:inset-x-0 before:dark:bg-darkmode-600/70 before:dark:border-darkmode-500/60">
               <FormInput
                 type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 className="block px-4 py-3"
                 placeholder="Email"
               />
               <FormInput
-                type="password"
+                type={showPassword ? "text" : "password"}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 className="block px-4 py-3 mt-4"
                 placeholder="Password"
               />
+              <button
+                onClick={toggleShowPassword}
+                className="block mt-2 text-xs text-slate-500"
+              >
+                {showPassword ? "Hide" : "Show"} Password
+              </button>
               <div className="flex mt-4 text-xs text-slate-500 sm:text-sm">
                 <div className="flex items-center mr-auto">
                   <FormCheck.Input
@@ -48,10 +111,28 @@ function Main() {
                 <a href="">Forgot Password?</a>
               </div>
               <div className="mt-5 text-center xl:mt-8 xl:text-left">
-                <Button variant="primary" className="w-full xl:mr-3">
-                  Login
+                <Button
+                  variant="primary"
+                  className="w-full xl:mr-3"
+                  onClick={handleSignIn}
+                  disabled={isLoading}
+                >
+                  {isLoading ? (
+                    <LoadingIcon
+                      icon="oval"
+                      className="w-8 h-8"
+                      color="white"
+                    />
+                  ) : (
+                    "Login"
+                  )}
                 </Button>
-                <Button variant="outline-secondary" className="w-full mt-3">
+                <Button
+                  variant="outline-secondary"
+                  className="w-full mt-3"
+                  onClick={navigateToSignUp}
+                  disabled={isLoading}
+                >
                   Sign up
                 </Button>
               </div>
